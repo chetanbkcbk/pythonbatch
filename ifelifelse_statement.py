07e89d9d4bf79660bf2821c22632581f443f4f18
Chetan <chetanbkcbk@gmail.com>
2025-10-17 17:47:23 +0530

    ifelifelse ladder

---
+++ ifelifelse_statement.py
@@ -0,0 +1,61 @@
+'''temp=int(input('enter the temperature'))
+
+
+if temp>=35:
+    print("Hot")
+elif temp>=25:
+    print("Warm")
+elif temp>=15:
+    print("cool")
+else:
+    print("very cool")
+
+print("last")
+'''
+
+marks=int(input('enter the marks scored with hardwork'))
+
+if marks<=100 and marks>=90:
+    print("A")
+elif marks<=89 and marks>=80:
+    print("B")
+elif marks<=79 and marks>=70:
+    print("C")
+elif marks<=69 and marks>=60:
+    print("D")
+else:
+    print("study hard party less")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
