c43dcdbaf9702409172758899b20ea18ce330f0a
Chetan <chetanbkcbk@gmail.com>
2025-10-22 12:37:05 +0530

    for loop on list

--- for_loop.py
+++ for_loop.py
@@ -1,3 +1,8 @@
 #creating for loop syntax
-for element in range(0,5):
-    print(element)
+'''for element in range(0,5):
+    print(element)'''
+
+#for loop on collection
+l=[10,20,30,40,50,60]
+for ele in l:
+    print(ele)
